fa3c0f78e3a8214699e3b435c3de844819e1def7
Dave <dave.cadelina@outlook.com>
2023-08-08 12:52:49 +1000

    created options icons for different categories

--- components/OurCourses/OurCourses.jsx
+++ components/OurCourses/OurCourses.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./OurCourses.module.scss";
+import OurCoursesOption from "../OurCoursesOption/OurCoursesOption";
 
 const OurCourses = () => {
 	return (
@@ -8,6 +9,29 @@ const OurCourses = () => {
 				<h1 className={styles.heading}>OUR COURSES</h1>
 			</div>
 			<h2 className={styles.subtitle}>Browse our popular courses</h2>
+			<p className={styles.message}>
+				Sed sit mauris habitant pellentesque leo ac donec sit interdum accumsan
+				morbi gravida hendrerit amet iaculis nibh nibh proin a vulputate turpis
+				amet.
+			</p>
+			<div className={styles.our_courses_options}>
+				<OurCoursesOption
+					url={"/icons/all-categories-icon-learnbook.svg"}
+					label={"All"}
+				/>
+				<OurCoursesOption
+					url={"/icons/design-category-icon-learnbook.svg"}
+					label={"All"}
+				/>
+				<OurCoursesOption
+					url={"/icons/development-category-icon-learnbook.svg"}
+					label={"All"}
+				/>
+				<OurCoursesOption
+					url={"/icons/marketing-category-icon-learnbook.svg"}
+					label={"All"}
+				/>
+			</div>
 		</div>
 	);
 };
